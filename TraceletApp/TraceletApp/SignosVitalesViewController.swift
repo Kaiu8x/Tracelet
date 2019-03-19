b67f34875254da8d82319013e8c00cddcff48447
Kenyiro Tsuru <kenyiro.tsuru@gmail.com>
2019-03-18 20:58:50 -0600

    Vistas y algunas clases para las vistas

---
+++ TraceletApp/TraceletApp/SignosVitalesViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignosVitalesViewController.swift
+//  TraceletApp
+//
+//  Created by kenyiro tsuru on 3/18/19.
+//  Copyright © 2019 Kai Kawasaki Ueda. All rights reserved.
+//
+
+import UIKit
+
+class SignosVitalesViewController: UIViewController {
+
+    @IBAction func desaparecerVista(_ sender: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
