e396d2a503ef04b9256294d1aa6dabd2a7f70161
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2019-03-20 17:17:38 -0600

    Revert "Json drill-down donde"
    This reverts commit e599910e97b921d01bedda94b5ff9e29cf93dca1.

---
+++ TraceletApp/TraceletApp/Base.lproj/AdministrarUsuariosViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AdministrarUsuariosViewController.swift
+//  TraceletApp
+//
+//  Created by kenyiro tsuru on 3/19/19.
+//  Copyright © 2019 Kai Kawasaki Ueda. All rights reserved.
+//
+
+import UIKit
+
+class AdministrarUsuariosViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToAdminUsuarios(sender: UIStoryboardSegue) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
