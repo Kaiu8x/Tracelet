95395b820c1feb7ebe75f54732466ef7dc7feff6
Kai Kawasaki Ueda <kaiu8x@Kais-MacBook-Pro.local>
2019-03-20 18:51:23 -0600

    3 niveles json implementado

--- TraceletApp/TraceletApp/UserDetailsEntregaViewController.swift
+++ TraceletApp/TraceletApp/UserDetailsEntregaViewController.swift
@@ -1,18 +1,21 @@
 //
-//  AdministrarUsuariosViewController.swift
+//  UserDetailsEntrega.swift
 //  TraceletApp
 //
 //  Created by Kai Kawasaki Ueda on 3/20/19.
 //  Copyright © 2019 Kai Kawasaki Ueda. All rights reserved.
 //
 
-
+import Foundation
 import UIKit
 
-class AdministrarUsuariosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+class UserDetailsEntregaViewController : UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
     
-    var dataFiltered = [Any]()
+    var newArray2:[Any]?
+    var newArray:[String]?
+    var dataFiltered = [String]()
     let searchController = UISearchController(searchResultsController: nil)
+    
     @IBOutlet weak var tableView: UITableView!
     
     func updateSearchResults(for searchController: UISearchController) {
@@ -20,8 +23,8 @@ class AdministrarUsuariosViewController: UIViewController, UITableViewDelegate, 
             dataFiltered = newArray!
         } else {
             dataFiltered = newArray!.filter{
-                let objectUser=$0 as! [String:Any]
-                let s:String = objectUser["name"] as! String;
+                let objectUser=$0
+                let s:String = objectUser ;
                 return(s.lowercased().contains(searchController.searchBar.text!.lowercased()))
                 
             }
@@ -29,10 +32,6 @@ class AdministrarUsuariosViewController: UIViewController, UITableViewDelegate, 
         
         self.tableView.reloadData()
     }
-    
-    let dataUrl = "http://martinmolina.com.mx/201911/data/jsons/users.json"
-    //let dataUrl = "http://martinmolina.com.mx/201813/data/datos.json"
-    var newArray:[Any]?
     
     func JSONParseArray(_ string: String) -> [AnyObject]{
         if let data = string.data(using: String.Encoding.utf8) {
@@ -50,13 +49,11 @@ class AdministrarUsuariosViewController: UIViewController, UITableViewDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = URL(string: dataUrl)
-        let data = try? Data(contentsOf: url!)
-        
-        
-        newArray = try! JSONSerialization.jsonObject(with: data!) as? [Any]
+        newArray = newArray2 as? [String]
+        print("new array")
         print(newArray)
+        print("new array2")
+        print(newArray2)
         dataFiltered = newArray!
         
         searchController.searchResultsUpdater = self
@@ -64,11 +61,12 @@ class AdministrarUsuariosViewController: UIViewController, UITableViewDelegate, 
         searchController.dimsBackgroundDuringPresentation = false
         
         searchController.hidesNavigationBarDuringPresentation = false
-       
+        
         definesPresentationContext = true
         
         tableView.tableHeaderView = searchController.searchBar
         
+        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
@@ -87,43 +85,40 @@ class AdministrarUsuariosViewController: UIViewController, UITableViewDelegate, 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell2", for: indexPath)
         
-        let objectUser = dataFiltered[indexPath.row] as! [String: Any]
-        let s:String = objectUser["name"] as! String
+        let objectUser = dataFiltered[indexPath.row]
+        //let s:String = objectUser["name"] as! String
         
-        cell.textLabel?.text=s
+        cell.textLabel?.text=objectUser
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var index = 0
-        var objectUser = [String:Any]()
-        let nextView = self.storyboard?.instantiateViewController(withIdentifier: "UserDetails") as! UserDetailsViewController
-        //let nextView = self.storyboard?.instantiateViewController(withIdentifier: "UserDetailsEntrega") as! UserDetailsEntregaViewController
+        var objectUser = String()
+        //let nextView = self.storyboard?.instantiateViewController(withIdentifier: "UserDetails") as! UserDetailsViewController
+        let nextView = self.storyboard?.instantiateViewController(withIdentifier: "UserDetailsEntrega2") as! UserDetailsEntrega2ViewController
         //Verificar si la vista actual es la de búsqueda
         if (self.searchController.isActive)
         {
             index = indexPath.row
-            objectUser = dataFiltered[index] as! [String: Any]
+            objectUser = dataFiltered[indexPath.row]
             
         }
             //sino utilizar la vista sin filtro
         else
         {
             index = indexPath.row
-            objectUser = newArray![index] as! [String: Any]
+            objectUser = (newArray?[indexPath.row])!
         }
-        let s:String = objectUser["name"] as! String
+        let s:String = objectUser
         
-        let sEnt:[String] = objectUser["canView"] as! [String]
-        print(sEnt)
-        //nextView.newArray2 = sEnt
+        print(s)
         nextView.name = s
+        //nextView.name = s
         
         self.navigationController?.pushViewController(nextView, animated: true)
     }
-    
-    
 }
