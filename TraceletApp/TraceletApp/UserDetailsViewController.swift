11e25f7b9ce0768070c888fdf039dac4191f1eb5
Kaiu8x <kawaswim@gmail.com>
2019-03-20 22:07:02 -0600

    merge complete

---
+++ TraceletApp/TraceletApp/UserDetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserDetailsViewController.swift
+//  TraceletApp
+//
+//  Created by Kai Kawasaki Ueda on 3/20/19.
+//  Copyright © 2019 Kai Kawasaki Ueda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserDetailsViewController : UIViewController {
+    
+    var name:String="XXX"
+    
+    @IBOutlet weak var theName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        theName.text = name
+        // Do any additional setup after loading the view.
+    }
+}
+
